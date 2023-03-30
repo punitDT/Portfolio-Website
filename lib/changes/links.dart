7ab6ffa3403465fc50ea8990f349195ae2f7ed60
PUNIT <punit.mece@gmail.com>
2023-03-30 16:51:40 +0530

    portfolio code

---
+++ lib/changes/links.dart
@@ -0,0 +1,37 @@
+// CV
+const String resume =
+    'https://docs.google.com/document/d/1GarEn-qc9vMS_zt9Z52zLKjLHlJBExMiEOodaT5JO0A/edit?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/punit1111';
+// Github Link
+//const String facebook = 'https://facebook.com/sudeshnb';
+// Github Link
+//const String twitter = 'https://twitter.com/sudesh78';
+// Github Link
+//const String youtube = 'https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/punit-patel-67906874';
+// Github Link
+//const String buymeacoffee = 'https://www.buymeacoffee.com/sudeshnb';
+// Github Link
+//const String instagram = 'https://instagram.com/sudesh_nish';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/sudeshnb",
+//   // "https://instagram.com/sudesh_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/sudesh78",
+//   "https://linkedin.com/in/sudeshnb",
+//   "https://www.buymeacoffee.com/sudeshnb", "https://github.com/sudeshnb",
+// ];
+//
+const String profileImage =
+    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+//
+// Github Link
+//const String fiverr = 'https://www.fiverr.com/sudeshnb';
+// Github Link
+const String upwork = 'https://www.upwork.com/freelancers/~01e0dff917f3bdff40';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=9724117174';
